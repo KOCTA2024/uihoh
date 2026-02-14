5e9493458274e8ba0622ae75ca20bcbb759494fb
aaalfl <aa@aa.com>
2026-02-14 17:36:08 +0200

    1

--- public/script.js
+++ public/script.js
@@ -1,5 +1,50 @@
+let jokes = document.querySelector(".jokes")
+
+function render() {
 fetch("http://localhost:3000/jokes")
 .then((res)=>res.json())
 .then(data=>{
     console.log(data)
+    jokes.innerHTML = ""
+    data.forEach(j=>{
+        jokes.innerHTML += joke(j)
+    })
 })
+}
+function joke(data){
+    return `<div class="joke">
+                <div class="content">
+                    ${data.content}
+                </div>
+                <div class="buttons">
+                    <div class="likes">${data.likes}</div>
+                    <button onclick="like(${data.id})" class="like"><i class="fa-solid fa-thumbs-up"></i></button>
+                    <div class="dislikes">${data.dislikes}</div>
+                    <button onclick="dislike(${data.id})"class="dislike"><i class="fa-solid fa-thumbs-down"></i></button>
+                </div>
+            </div>`
+}
+
+
+function like(id){
+    fetch("http://localhost:3000/like?id=" + id).then(res=> res.status == 200 && render())
+}
+
+function dislike(id){
+    fetch("http://localhost:3000/dislike?id=" + id).then(res=> res.status == 200 && render())
+}
+
+document.querySelector("form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    let content = e.target["content"].value
+    console.log(content)
+    if(content.length < 3){
+        alert("😂🤣🤣🤣")
+        return
+    }
+    fetch("http://localhost:3000/jokes", {
+        method : "POST",
+        body: JSON.stringify({content})
+    }).then(res=> res.status == 200 && render())
+})
+render()
